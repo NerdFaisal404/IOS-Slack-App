bb5cf818a028bcf0c85caf0d505fefca36177022
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-07-09 23:39:25 +0600

    navigation added

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SlackApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SlackApp
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'Alamofire', '~> 5.0.0-beta.5'
+  pod 'Socket.IO-Client-Swift', '~> 15.1.0'
+end
---
+++ SlackApp/Controller/ChatVc.swift
@@ -0,0 +1,21 @@
+//
+//  ChatVc.swift
+//  SlackApp
+//
+//  Created by Faisal Ahmed on 7/9/19.
+//  Copyright © 2019 Faisal Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ChatVc: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
